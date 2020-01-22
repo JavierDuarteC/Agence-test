730842a7f4f929e771d43c9b88e5b3333b6ca14b
Javier Duarte <javierandresduarte@gmail.com>
2020-01-21 22:37:14 -0500

    Relatorio done

--- app/routes/relatorio.js
+++ app/routes/relatorio.js
@@ -5,13 +5,14 @@ const { Op } = require("sequelize");
 const {
     cao_os_model,
     cao_fatura_model,
+    cao_salario_model
 } = require(__dirname + '/../config/db')
 //Associate tables by FK
 
 cao_os_model.hasMany(cao_fatura_model, { foreignKey: 'co_os' })
 cao_fatura_model.belongsTo(cao_os_model, { foreignKey: 'co_os' })
 
-var usernames = ['anapaula.chiodaro', 'renato.pereira']
+var usernames = ['anapaula.chiodaro', 'renato.pereira','carlos.carvalho']
 var dates = ['2007-01', '2010-02']
 var data = {}
 //Transform dates
@@ -48,22 +49,37 @@ router.route('/').get((req, res) => {
             }]
         })
             .then(faturas => {
-                //Faturas ordered by owner
-                data[user] = {};
-                faturas.forEach(item => {
-                    var emissaoDate = item.data_emissao.split('-');
-                    var stringDate = emissaoDate[0]+'-'+emissaoDate[1];
-                    data[user][stringDate]= data[user][stringDate] || {
-                        receita_liquida: 0,
-                        custo_fixo: 0,
-                        comissao: 0,
-                        lucro: 0
-                    };
-                    data[user][stringDate].receita_liquida += item.valor
-                    data[user][stringDate].custo_fixo += item.valor
-                    data[user][stringDate].comissao += item.valor
-                    data[user][stringDate].lucro += item.valor
-                });
+                //If user has faturas
+                if (faturas.length > 0) {
+                    //Find salario from user
+                    cao_salario_model.findOne({
+                        where:{
+                            co_usuario: user
+                        }
+                    }).then(salario => {
+                        //Load data with Faturas info ordered by owner
+                        data[user] = {};
+                        faturas.forEach(item => {
+                            var emissaoDate = item.data_emissao.split('-');
+                            var stringDate = emissaoDate[0] + '-' + emissaoDate[1];
+                            //group by date (year-month) create new propery if it doesnt exist and set custo_fixo with users salario
+                            if (!data[user].hasOwnProperty(stringDate)) {
+                                data[user][stringDate] = {
+                                    receita_liquida: 0.0,
+                                    custo_fixo: salario.brut_salario,
+                                    comissao: 0.0,
+                                    lucro: 0.0
+                                };
+                            }
+                            //Add values to each property grouped by date, set temp vars
+                            var rl = data[user][stringDate].receita_liquida += (item.valor - (item.total_imp_inc * item.valor / 100))
+                            var cm = data[user][stringDate].comissao += data[user][stringDate].receita_liquida*item.comissao_cn/100
+                            data[user][stringDate].lucro += rl-(data[user][stringDate].custo_fixo+cm)
+                        });
+                    })
+                        .catch(err => res.status(400).json('Error: ' + err))
+                }
+                //End await foreach
                 done()
             })
             .catch(err => res.status(400).json('Error: ' + err))
